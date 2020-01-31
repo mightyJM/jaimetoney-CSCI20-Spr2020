ec2b5b0722c37aa7d2e31fb82387120391c1ba84
mightyJM <mighty.jaime.toney@gmail.com>
2020-01-31 03:30:50 +0000

    Added section 1- You Code It empty section for fill in

---
+++ maing.go
@@ -0,0 +1,14 @@
+// Programmer name: __________
+// Date completed:  __________
+// Description: ___________________________
+
+package main
+
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println("My name is __________, and my major is __________.")
+	fmt.Println("This is my __________ semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is __________.")
+}
