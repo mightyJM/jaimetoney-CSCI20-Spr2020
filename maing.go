d4a8d861976389f3c42659ebec46bc825b271f6b
mightyJM <mighty.jaime.toney@gmail.com>
2020-01-31 03:32:14 +0000

    Added personal information to template

--- maing.go
+++ maing.go
@@ -8,7 +8,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
-	fmt.Println("My name is __________, and my major is __________.")
-	fmt.Println("This is my __________ semester at Butte College.")
-	fmt.Println("One of my favorite pastimes is __________.")
+	fmt.Println("My name is Jaime Ann, and my major is Computer Science.")
+	fmt.Println("This is my sixth semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is hanging out with my wife and getting outdoors.")
 }
